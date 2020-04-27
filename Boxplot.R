5af7d197429607a17c7baa5be4758ae31846ed85
Roger <59595695+RogerHTang@users.noreply.github.com>
2020-04-28 05:59:19 +1000

    Add files via upload
    An example of a scientific publication standard boxplot.

---
+++ Boxplot.R
@@ -0,0 +1,19 @@
+
+library(ggplot2)
+library(extrafont)
+font_import()
+y
+loadfonts(device="win") # for windows
+
+ggplot(<dataset>, aes(x = <species_collumn>, y = <metal>))+
+   geom_boxplot(coef=1.0, outlier.shape = 1, outlier.size = 2, fill="white", color = "dark red")+
+   ylim(0, 17000)+ # change the second number to the rounded up number of your maximum value 
+   geom_hline(yintercept = <hyperaccumulation_threshold>, color = "black",linetype="dashed")+ #This line is optional if you want to include a threshold
+   theme_bw() +
+   xlab("") +
+   ylab(bquote("<inset_metal> ("*mu*"g"*~g^-1*")"))+
+   scale_x_discrete(labels= <x.axis_labels>)+
+   coord_flip()+ #delete line to flip chart
+   theme(text=element_text(family="Times New Roman"),
+         axis.text=element_text(color = "black", size=12),
+         axis.text.y=element_text(face="italic")) # delete line if species are not used
